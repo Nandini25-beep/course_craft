38d4523fa91fb8840415175388d582bb50196820
NandiniMedisetti <nandinimedisetti50@gmail.com>
2026-09-23 10:09:33 +0530

    server code

--- server/app.js
+++ server/app.js
@@ -1,12 +1,22 @@
 const express = require("express")
+const connectDB =require("./config/db")
+const dns = require("dns")
+const dotenv = require("dotenv")
+const courseRoute = require("./routes/courseRoutes")
+const authRoute = require("./routes/authRoutes")
+
+dns.setServers(["1.1.1.1","8.8.8.8"])
+dotenv.config()
 const app = express()
+app.use(express.json())
 
 
 app.get("/welcome",(req,res)=>{
     res.send("welcome back")
 })
-
-
+app.use("/api/auth",authRoute)
+app.use("/api/courses",courseRoute)
+connectDB()
 
 app.listen(3000,()=>{
     console.log("listening to the PORT")
